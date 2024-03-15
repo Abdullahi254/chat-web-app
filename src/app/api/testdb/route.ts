1ce23b07eab3523fda20fbe2b877cbce04061782
Abdullahi254 <abdush504@gmail.com>
2024-03-15 21:58:06 +0300

    Response

--- src/app/api/testdb/route.ts
+++ src/app/api/testdb/route.ts
@@ -1,9 +1,7 @@
 import { dbConnect, disconnect } from "@/app/lib/db";
-import { NextResponse } from "next/server";
 
 export async function GET() {
     await dbConnect();
     console.log("hit db connect", new Date().getSeconds());
-
-    return NextResponse.json({ messsage: "Hello World! Connection successfull" });
+    return Response.json({ messsage: "Hello World! Connection successfull" });
 }
