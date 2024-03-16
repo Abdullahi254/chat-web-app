8263f8bae9ac6f8beeab808d5e9456e163b75b76
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-03-16 16:51:08 +0200

    feat: add socket.io feature. - Send & emit events using socket.io.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import ChatScreen from "@/components/ChatScreen";
 import SideChat from "@/components/SideChat";
 
+import { io } from 'socket.io-client';
+import { useState, useEffect } from 'react'
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="grid grid-cols-3 h-screen py-10 px-6 max-w-7xl mx-auto">
