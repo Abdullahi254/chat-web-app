041fa0f6b8626d811a72443064ee5d4ccd811a5d
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-03-18 19:57:27 +0200

    chore: tmp commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import { io } from 'socket.io-client';
 import { useState, useEffect } from 'react'
 import Image from "next/image";
 export default function Home() {
-    const client_socket = io('http://localhost:4000');
+    const client_socket = io('http://localhost:5000');
     const [isConnected, setIsConnected] = useState(client_socket.connected);
     const [sendMsg, setSendMsg] = useState(false);
     const sendServerMsg = () => {
         client_socket.emit('msg', { msg: 'Hello from Client' })
     }
+    //TODO: Research on how to use hooks in react for sockets.
     //TODO: Ensure useEffect doesnt keep requesting on connections
     useEffect(() => {
         client_socket.on("connect", () => {
