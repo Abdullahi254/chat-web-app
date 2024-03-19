6126a154cdf357fc580f4ee3002a088851cb98af
Abdullahi254 <abdush504@gmail.com>
2024-03-19 15:48:38 +0300

    resolved merge conflict

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const express = require('express')
-const app = express()
-const port = 5000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-=======
 // entry to server
 const express = require('express');
 const routes = require('./routes');
@@ -30,4 +17,3 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
->>>>>>> c8ff33b (updated authentication)
