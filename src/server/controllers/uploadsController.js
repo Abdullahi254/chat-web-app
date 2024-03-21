468cf968518d9550a2e35426705a8e4023014744
vierukundo <vierukundo@gmail.com>
2024-03-21 12:33:24 +0200

    Created upload aPI

---
+++ src/server/controllers/uploadsController.js
@@ -0,0 +1,21 @@
+const dbClient = require('../utils/db');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + '/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const uploads = multer({ storage: storage });
+
+const uploadFile = async (req, res) => {
+    console.log(req.files);
+    res.send('File uploaded');
+}
+
+
+module.exports = { uploadFile, uploads };
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { registerUser, loginUser, verifyToken } = require("../controllers/UserController");
+const { uploadFile, uploads } = require("../controllers/uploadsController");
 
 
 const router = express.Router();
@@ -7,6 +8,7 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/verify_token", verifyToken);
+router.post("/upload", uploads.array('files'), uploadFile);
 
 
 module.exports = router;
