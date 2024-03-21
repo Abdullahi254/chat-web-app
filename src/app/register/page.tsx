784ecc23b6ccf756ae2621b1c7832495b2222b12
Abdullahi254 <abdush504@gmail.com>
2024-03-21 13:30:31 +0300

    feature to view or unview password when logging in or registering

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import { handleRegistration } from '@/lib/actions'
+import { useState } from 'react';
 import { useFormState, useFormStatus } from 'react-dom'
+import { FaRegEyeSlash } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
 
 
 const Page = () => {
     const [errorMessage, dispatch] = useFormState(handleRegistration, undefined)
+    const [show, setShow] = useState<boolean>(false)
 
     return (
         <main className="flex flex-col space-y-4 min-h-screen items-center py-20 px-6 max-w-4xl mx-auto">
@@ -17,13 +21,22 @@ const Page = () => {
                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
                     <input name="email" id="email" type='email' className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Email" required />
                 </div>
-                <div>
+                <div className='relative'>
                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                    <input type={show ? "text" : "password"} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                    {
+                        show ? <IoEyeSharp className='absolute right-3 top-10 cursor-pointer' onClick={() => setShow(prev => !prev)} /> :
+                            <FaRegEyeSlash className='absolute right-3 top-10 cursor-pointer' onClick={() => setShow(prev => !prev)} />
+                    }
                 </div>
-                <div>
-                    <label htmlFor="password2" className="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
-                    <input type="password" name="password2" id="password2" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                <div className='relative'>
+                    <label htmlFor="password2" className="block mb-2 text-sm font-medium text-gray-900">Confirm </label>
+                    <input type={show ? "text" : "password"} name="password2" id="password2" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                    {
+                        show ? <IoEyeSharp className='absolute right-3 top-10 cursor-pointer' onClick={() => setShow(prev => !prev)} /> :
+                            <FaRegEyeSlash className='absolute right-3 top-10 cursor-pointer' onClick={() => setShow(prev => !prev)} />
+                    }
+
                 </div>
                 <div>{errorMessage && <p className='text-sm font-medium text-red-500'>{errorMessage}</p>}</div>
                 <RegisterButton />
@@ -34,7 +47,7 @@ const Page = () => {
 
 function RegisterButton() {
     const { pending } = useFormStatus()
-   
+
     return (
         <button aria-disabled={pending} type="submit" className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Register</button>
     )
