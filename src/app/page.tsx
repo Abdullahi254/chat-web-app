98ed0343379e20c3ed5a1662825b5479a5f1ed4f
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-03-21 12:16:46 +0200

    chore: remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import ChatScreen from "@/components/ChatScreen";
 import SideChat from "@/components/SideChat";
 import Image from "next/image";
 
-import { io } from 'socket.io-client';
 import { useState, useEffect } from 'react'
 export default function Home() {
-  // check the token if is valid
-  return (
-    <main className="grid grid-cols-3 h-screen py-10 px-6 max-w-7xl mx-auto">
-      <SideChat/>
-      <ChatScreen/>
-    </main>
-  );
+    // check the token if is valid
+    return (
+        <main className="grid grid-cols-3 h-screen py-10 px-6 max-w-7xl mx-auto">
+            <SideChat />
+            <ChatScreen />
+        </main>
+    );
 }
