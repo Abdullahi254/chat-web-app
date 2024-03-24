01df0082cefa9eb18c276cbd37099d1841c37462
vierukundo <vierukundo@gmail.com>
2024-03-24 16:35:44 +0200

    fixed token error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,43 @@
 import ChatScreen from "@/components/ChatScreen";
 import SideChat from "@/components/SideChat";
-import { cookies } from 'next/headers'
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const getSession = async (token?: string) => {
+  if (!token) {
+    console.log("Token is invalid");
+    return false;
+  }
   try {
-    const res = await fetch(`${process.env.REACT_APP_BASE_URL}/verify_token?token=${token}`)
+    const res = await fetch(
+      `${process.env.REACT_APP_BASE_URL}/verify_token?token=${token}`
+    );
     if (res.status === 200) {
-      console.log("Token is valid")
-      return res.json()
-    }
-    else {
-      console.log("Token is invalid")
-      return false
+      console.log("Token is valid");
+      return res.json();
+    } else {
+      console.log("Token is invalid");
+      return false;
     }
   } catch (er) {
     if (er instanceof Error) {
-      console.log(er.message)
+      console.log(er.message);
+    }
+    throw er;
   }
-  throw er
-  }
-}
+};
 
 export default async function Home() {
-  const cookieStore = cookies()
-  const token = cookieStore.get('x-token')?.value
-  const results = await getSession(token)
-  if (!results) redirect("/login")
-  const userId = results.userId["id"]
+  const cookieStore = cookies();
+  const token = cookieStore.get("x-token")?.value;
+  const results = await getSession(token);
+  if (!results) redirect("/login");
+  const userId = results.userId["id"];
   // const userId = results.useId.id
   return (
     <main className="grid grid-cols-3 min-h-screen py-10 px-6 max-w-7xl mx-auto overflow-hidden">
       <SideChat />
       <ChatScreen userId={userId} />
     </main>
-  )
-};
-
+  );
+}
