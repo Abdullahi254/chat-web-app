177a2888f98606bd303280ba9ff5ac38547f9b08
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-03-26 16:12:33 +0200

    fix: specify 'id' in params since folder name is [id]

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -3,20 +3,20 @@ import ChatScreen from '@/components/ChatScreen'
 import React from 'react'
 
 type Props = {
-  params:{
-    chatId: string
-  }
+    params: {
+        id: string
+    }
 }
 
 //fetch text thread from database use chatId to get the data
 
-const page = ({params}: Props) => {
-  return (
-    <main className="grid grid-cols-3 min-h-screen py-10 px-6 max-w-7xl mx-auto">
-      <SideChat/>
-      <ChatScreen chatId={params.chatId} />
-    </main>
-  )
+const page = ({ params }: Props) => {
+    return (
+        <main className="grid grid-cols-3 min-h-screen py-10 px-6 max-w-7xl mx-auto">
+            <SideChat />
+            <ChatScreen chatId={params.id} />
+        </main>
+    )
 }
 
 export default page
