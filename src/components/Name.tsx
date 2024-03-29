2716e5182c97558823f7adc0cf52b71bb6e1feab
Abdullahi254 <abdush504@gmail.com>
2024-03-30 01:15:24 +0300

    updated user name functionality

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,13 +1,12 @@
 "use client"
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { CiEdit } from "react-icons/ci";
-import { useFormStatus, useFormState  } from 'react-dom'
+import { useFormStatus, useFormState } from 'react-dom'
 import { TiTick } from "react-icons/ti";
-import { MdCancel } from "react-icons/md";
-import {updateName} from "@/lib/actions"
+import { updateName } from "@/lib/actions"
 import { BioData } from './ProfileCard';
 type Props = {
-    userId:string
+    userId: string
     bioData?: BioData
 }
 
@@ -24,21 +23,22 @@ const Name = ({ userId, bioData }: Props) => {
                         <p className='text-sm text-gray-900'>{bioData?.username}</p>
                         <span className='cursor-pointer' onClick={() => setShowInput(false)}><CiEdit /></span>
                     </div> :
-                    <div className='flex items-center space-x-2'>
-                        <form className='flex items-center space-x-2' action={dispatch}>
-                            <input type="text" id="name" name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block ps-10 p-2.5 flex-1" placeholder="New Name" />
-                            <input type="text" id="userId" name="userId" defaultValue={userId} hidden />
-                            <button type='submit' aria-disabled={pending}>
-                                <TiTick className='hover:text-green-500 text-lg text-gray-700 disabled:text-gray-50' />
-                            </button>
-                        </form>
-                        <button onClick={() => {
-                            if (!pending) {
-                                setShowInput(true)
-                            }
-                        }} aria-disabled={pending}>
-                            <MdCancel className='text-red-500 text-lg disabled:text-gray-50' />
-                        </button>
+                    <div>
+                        <div className='flex items-center space-x-2'>
+                            <form className='flex items-center space-x-2' action={dispatch}>
+                                <input type="text" id="name" name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block ps-10 p-2.5 flex-1" placeholder="New Name" />
+                                <input type="text" id="userId" name="userId" defaultValue={userId} hidden />
+                                <button type='submit' aria-disabled={pending}>
+                                    <TiTick className='hover:text-green-500 text-lg text-gray-700 disabled:text-gray-50' />
+                                </button>
+                            </form>
+                        </div>
+                        {
+                            !errorMessage?.isError && <p className='py-2 w-full text-center text-xs tracking-wide text-green-500'>{errorMessage?.message}</p>
+                        }
+                        {
+                            errorMessage?.isError && <p className=' py-2 w-full text-center text-xs tracking-wide text-red-500'>{errorMessage?.message}</p>
+                        }
                     </div>
             }
         </>
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -126,18 +126,54 @@ export const handleAddMember = async (
   }
 };
 
-export const updateAbout = async (chatId:string, formData:FormData)=>{
+export const updateAbout = async (chatId: string, formData: FormData) => {
   console.log(formData.get("about"))
-  return{
+  return {
 
   }
 }
 
-export const updateName = async(  _currentState: unknown,
-  formData: FormData,)=>{
-  console.log("ID:", formData.get("userId") )
+export const updateName = async (_currentState: unknown,
+  formData: FormData,) => {
+  console.log("ID:", formData.get("userId"))
   console.log("NAME:", formData.get("name"))
-  return {
-
+  const userId = formData.get("userId")
+  const newName = formData.get("name")
+  try {
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_BASE_URL + "/edit_username",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId,
+          newName,
+        }),
+      },
+    );
+    const resul = await res.json()
+    if (res.ok) {
+      if (resul.Error) {
+        return {
+          isError: true,
+          message: resul.Error,
+        };
+      } else {
+        return {
+          isError: false,
+          message: "Successfully Changed Name!",
+          newName: resul.newName
+        }
+      }
+    } else {
+      throw Error('')
+    }
+  } catch (error) {
+    return {
+      isError: true,
+      message: "Error Changing Name!"
+    }
   }
 }
